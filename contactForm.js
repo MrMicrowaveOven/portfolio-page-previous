4273a2c7637f4eda890a11c0981dd60c199806d7
Benjamin <benj@zagorski.com>
2017-01-16 12:16:58 -0800

    Changed form format entirely

--- contactForm.js
+++ contactForm.js
@@ -1,24 +1,27 @@
-function sendForm() {
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var contactBody = document.getElementById("contactBody").value;
-  console.log("Sending to " + name + ", " + email + ", " + contactBody + ".");
-  $.ajax({
-    url: "https://formspree.io/benj@zagorski.com",
-    method: "POST",
-    data: {
-      name: name,
-      _replyto: email,
-      _subject: "Someone likes your portfolio!",
-      message: contactBody
-    },
-    dataType: "json",
-    success: function() {
-      $("#submit-success").fadeIn();
-      $("#contact-form").fadeOut();
-    },
-    error: function() {
-      $("#submit-errors").fadeIn();
-    }
-  });
-}
+$(document).ready(function() {
+  $('#contact-form').submit(function(e) {
+      var name = $('#inputName')
+      var email = $('#inputEmail')
+      var message = $('#inputMessage')
+
+      if(name.val() == "" || email.val() == "" || message.val() == "") {
+        $('.submit-fail').fadeToggle(400);
+        return false;
+      }
+      else {
+        $.ajax({
+          method: 'POST',
+          url: '//formspree.io/your@email.com',
+          data: $('#contact-form').serialize(),
+          datatype: 'json'
+        });
+        e.preventDefault();
+        $(this).get(0).reset();
+        $('.submit-success').fadeToggle(400);
+      }
+    });
+
+  $('.submit-fail, .submit-success').click(function() {
+    $(this).hide();
+  })
+});
