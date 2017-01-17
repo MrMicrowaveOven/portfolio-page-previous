de41fa07f221bc154018f88a4b7f39c7f2723b7e
Benjamin <benj@zagorski.com>
2017-01-16 21:00:54 -0800

    Added 5-second fadeOut to all alerts

--- contactForm.js
+++ contactForm.js
@@ -6,7 +6,11 @@ $(document).ready(function() {
 
       if(name.val() == "" || email.val() == "" || message.val() == "") {
         console.log("Failure!!!");
-        $('.submit-fail').fadeToggle(400);
+        $('.submit-fail').fadeIn(400);
+        $('.submit-success').fadeOut(400);
+        setTimeout(function() {
+          $('.submit-fail').fadeOut(400);
+        }, 5000);
         return false;
       }
       else {
@@ -22,9 +26,14 @@ $(document).ready(function() {
       $('.submit-fail').fadeOut(400);
       e.preventDefault();
       $(this).get(0).reset();
+      setTimeout(function() {
+        $('.submit-success').fadeOut(400);
+      }, 5000);
   });
 
   $('.submit-fail, .submit-success').click(function() {
     $(this).hide();
   });
+
+
 });
